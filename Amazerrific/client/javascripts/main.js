47488e30f769ec7e94c69024952f16de541d47b2
Gilliano Menezes <g.silva.de.menezes@avanade.com>
2018-07-27 14:14:58 -0300

    improve tabs behavior and create 'add' tab

--- Amazerrific/client/javascripts/main.js
+++ Amazerrific/client/javascripts/main.js
@@ -1,19 +1,55 @@
 var main = function(){
     "use strict";
+    
+    loadTabs();
+}
+
+var loadTabs = function(){
+    createAddTab();
 
     $.get('tags', function(tags){
         var $tabs = $('.tabs');
         tags.forEach((tag)=>{
             var $tab = $('<a>').attr('href','#').append($('<span>').text(tag));
-            $tabs.append($tab);
-            $tab.on("click", function(e){
-                var tag = e.target.innerText;
+            $tabs.append($tab);            
+        });    
+        
+        setTabClickEvent();
+    });
+}
+
+var createAddTab = function(){
+    var $span = $('<span>').text('Add');
+    $span.addClass('active add-tab')
+    var $tab = $('<a>').attr('href', '#').append($span);
+    $('.tabs').append($tab);
+    writeAddTabContent();
+}
+
+var setTabClickEvent = function(){
+    var $tabs = $('.tabs').find('span').toArray();
+    $tabs.forEach((tab) =>{
+        $(tab).on("click", function(e){
+            var tag = e.target.innerText;
+            setActive(e.target);
+
+            if($(tab).hasClass('add-tab')){
+                writeAddTabContent();
+            }else{
                 $.get('findByTag/' + tag , function(todoObjects){
                     writeContent(todoObjects);
                 })
-            })
-        });        
+            }
+        })
+    })
+}
+
+var setActive = function(tab){
+    var $spanTabs = $('.tabs').find('span').toArray();
+    $spanTabs.forEach((spanTab) =>{
+        $(spanTab).removeClass('active');
     });
+    $(tab).addClass('active');
 }
 
 var writeContent = function(todoObjects){
@@ -24,6 +60,18 @@ var writeContent = function(todoObjects){
     });
 
     $('.content').append($content);
+}
+
+var writeAddTabContent = function(){
+    $('.content').empty();
+    var $descriptionLabel = $('<label>').text('Descrição:');    
+    var $inputDescription = $('<input>');
+    var $tagslabel = $('<label>').text('Tags:');
+    var $inputTags = $('<input>');
+    $('.content').append($descriptionLabel);
+    $('.content').append($inputDescription);
+    $('.content').append($tagslabel);
+    $('.content').append($inputTags);
 }
 
 $(document).ready(main);
